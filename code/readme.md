3188e9d3d12b806ed1f539f016d021e9356b54ca
shivasamala <shivasamala.gm@gmail.com>
2026-02-19 11:44:54 +0530

    new

---
+++ code/readme.md
@@ -0,0 +1,10 @@
+This keyword value across different functions 
+
+Global - Window
+function - window
+method with es5 fnc-object
+method with es6 fnc -window
+es5 function inside es5 method - window
+arrow function inside es5 method - object
+event handler - element
+
--- code/this.js
+++ code/this.js
@@ -41,3 +41,15 @@ class Abcd{
 }
 let val = new Abcd()
 console.log(val)
+
+// This keyword value across different functions 
+
+// Global - Window
+// function - window
+// method with es5 fnc-object
+// method with es6 fnc -window
+// es5 function inside es5 method - window
+// arrow function inside es5 method - object
+// event handler - element
+//class - blank Object
+
